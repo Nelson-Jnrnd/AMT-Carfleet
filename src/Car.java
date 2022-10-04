581718e294ebe24b03fcde8453591029e26df675
VincentPeer <vincent.peer@heig-vd.ch>
2022-10-04 17:55:27 +0200

    debut test

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,15 @@
-public class Car {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Map;
+
+public class Car extends Data {
+
+    public Car() {
+
+    }
+
+    @JsonProperty("id")
+    String id;
+    @JsonProperty("name")
+    String name;
+
 }
--- test/TestCar.java
+++ test/TestCar.java
@@ -1,3 +1,26 @@
-public class TestCar {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import org.junit.jupiter.api.Test;
+import javax.print.DocFlavor;
+import java.io.File;
+import java.io.IOException;
+
+
+public class TestCar extends Data {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public TestCar() throws IOException {
+        Car car = objectMapper.readValue(new File("test/smallData.json"), Car.class);
+
+    }
+
+    @Test
+    public void serialize_ValidJson_Success() {
+        return;
+    }
+
+
+
 
 }
