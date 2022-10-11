5c1ce22852772515ea4ceb56ae80cf6ab668e68b
Nelson-Jnrnd <79466777+Nelson-Jnrnd@users.noreply.github.com>
2022-10-11 10:08:46 +0200

    car tests

--- test/TestCar.java
+++ test/TestCar.java
@@ -1,20 +1,62 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 
-public class TestCar extends Data {
-    ObjectMapper objectMapper = new ObjectMapper();
+public class TestCar {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    static final int CORRECT_ACCOUNT_ID = 7650377;
+    static final String CORRECT_CAR_ID = "939948275";
+    static final String CORRECT_CAR_NAME = "GE 123201";
+    static final Map<String, String> CORRECT_COLUMN_VALUES = Map.of(
+            "Modèle", "Volkswagen California",
+            "Nom/prénom", "Nelson De Bleser",
+            "Continent", "Europe"
+    );
+    private static Example testCarData;
 
-    public TestCar() throws IOException {
-        Example car = objectMapper.readValue(new File("test/smallData.json"), Example.class);
-        assert car.getAccountId() == 7650377;
+    @BeforeClass
+    public static void beforeClass() throws IOException {
+        testCarData = MAPPER.readValue(new File("test/smallData.json"), Example.class);
     }
 
     @Test
-    public void serialize_ValidJson_Success() {
-        return;
+    public void DeserializedCarHasCorrectAccountId() {
+        assert testCarData.getAccountId() == CORRECT_ACCOUNT_ID;
+    }
+
+    @Test
+    public void DeserializedCarHasCorrectId() {
+        for (Board board : testCarData.getData().getBoards()) {
+            for (Item item : board.getItems()) {
+                assert item.getId().equals(CORRECT_CAR_ID);
+            }
+        }
+    }
+
+    @Test
+    public void DeserializedCarHasCorrectName() {
+        for (Board board : testCarData.getData().getBoards()) {
+            for (Item item : board.getItems()) {
+                assert item.getName().equals(CORRECT_CAR_NAME);
+            }
+        }
+    }
+
+    @Test
+    public void DeserializedCarHasCorrectColumnValues() {
+
+        for (Board board : testCarData.getData().getBoards()) {
+            for (Item item : board.getItems()) {
+                for (ColumnValue columnValue : item.getColumnValues()) {
+                    assert columnValue.getText().equals(CORRECT_COLUMN_VALUES.get(columnValue.getTitle()));
+                }
+            }
+        }
     }
 
 
